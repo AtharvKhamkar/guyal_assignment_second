9ada17bd5e422d2097e48054dab434753674bdb7
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-07-17 15:08:32 +0530

    add swagger docs

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import userRoutes from "./Routes/user.routes.js";
 
+dotenv.config({
+    path: './env'
+})
+
 const app = express();
+console.log(process.env.PORT);
 const PORT = process.env.PORT || 2727;
 
 app.use(cors({
@@ -21,6 +29,13 @@ app.use(bodyParser.json());
 app.use(morgan("dev"))
 app.use('/api', userRoutes);
 
+const swaggerJsDocs = YAML.load("src/api.yaml")
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerJsDocs)
+)
 
 
 
